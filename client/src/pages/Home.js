b0a194b26da7997f1e10e5b1364eca5ef4ace85a
Siddhant Raj <siddhantraj04@gmail.com>
2023-05-29 13:19:43 +0530

    Modify Login Page to display error or redirect

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,38 +1,33 @@
-import React from 'react';
+import React from "react";
 // import './App.css';
-import {useEffect,useState} from "react";
-import axios from 'axios';
-import {useNavigate} from "react-router-dom"
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 const Home = () => {
-    const [listofpost,setListofposts]=useState([]);
-    const navigate = useNavigate();
-    
-    useEffect(()=>{
-      axios.get("http://localhost:3024/").then((response)=>{
-        // console.log(response);
-        setListofposts(response.data);
-        // console.log(response.data);
-      })
-    },[])
-  
+  const [listofpost, setListofposts] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios.get("http://localhost:3024/").then((response) => {
+      // console.log(response);
+      setListofposts(response.data);
+      // console.log(response.data);
+    });
+  }, []);
+
   return (
     <>
-    {listofpost.map((post)=>
+      {listofpost.map((post) => (
         <>
-  
-          
-          
-            <div className='post' onClick={()=>navigate(`/post/${post.id}`)}>
-              <div className='title'>{post.title}</div>
-              <div className='body'>{post.postText}</div>
-              <div className='footer'>{post.username}</div>
-            </div>
-          
-        </> 
-  
-    )}
+          <div className="post" onClick={() => navigate(`/post/${post.id}`)}>
+            <div className="title">{post.title}</div>
+            <div className="body">{post.postText}</div>
+            <div className="footer">{post.username}</div>
+          </div>
+        </>
+      ))}
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,35 +1,50 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-import axios from "axios"
 const Login = () => {
   const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-  const[username,setUsername]=useState("");
-  const[password,setPassword]=useState("");
-  const [auth,setAuth]=useState(-1);
-  const login=()=>{
-
-    axios.post("http://localhost:3024/users/login",{Username:username,Password:password}).then((response)=>{
-      console.log(response.data);
-      setAuth(response.data.status);
-      // setListofposts(response.data);
-    })
-  }
-  if(auth===0)
-  {
-    navigate("/");
-  }  
+  const login = () => {
+    axios
+      .post("http://localhost:3024/users/login", {
+        Username: username,
+        Password: password,
+      })
+      .then((response) => {
+        if (response.data.error) {
+          setError(response.data.error);
+        } else if (response.data.accessToken) {
+          sessionStorage.setItem("accessToken", response.data.accessToken);
+          navigate("/");
+        }
+      })
+      
+  };
 
   return (
-    
-    <div className='loginContainer'>
-      {auth===1 && <h3>Incorrect Username or password</h3>}
-      <input type="text" value={username} placeholder="Your username.." onChange={(event)=>setUsername(event.target.value)}/>
-      <input type="password" value={password} placeholder="Your password.." onChange={(event)=>setPassword(event.target.value)}/>
+    <div className="loginContainer">
+      {error && <h3>{error}</h3>}
+      <label>Username:</label>
+      <input
+        type="text"
+        value={username}
+        placeholder="Your username.."
+        onChange={(event) => setUsername(event.target.value)}
+      />
+      <label>Password:</label>
+      <input
+        type="password"
+        value={password}
+        placeholder="Your password.."
+        onChange={(event) => setPassword(event.target.value)}
+      />
       <button onClick={login}>Login</button>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
