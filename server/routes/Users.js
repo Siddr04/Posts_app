5e133e6bc5b1d75e7ed5b6f6faf9283f54a733a3
Siddhant Raj <siddhantraj04@gmail.com>
2023-05-29 13:20:32 +0530

    Create JWT web_authentication

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -8,6 +8,7 @@ const db = mysql.createPool({
   password: "",
   database: "Posts_app",
 });
+const {sign} = require('jsonwebtoken');
 
 const router = express.Router();
 router.post("/registration", async (req, res) => {
@@ -37,16 +38,21 @@ router.post("/registration", async (req, res) => {
 router.post("/login", async (req, res) => {
   const Username = req.body.Username;
   const password = req.body.Password;
-  const sqlfetch="Select password from Users where username= ?;";
+  const sqlfetch="Select password,Uid from Users where username= ?;";
   db.query(sqlfetch, [Username], async (err, result) => {
     if (
       result.length === 0 ||
       !(await bcrypt.compare(password, result[0].password))
     ) {
-      res.json({status:1});
+      res.json({error:"Incorrect User name or password!!"});
     } else {
       // const id=results[0].id;
-      res.json({status:0});
+      const accessToken=sign({username:Username,id:result[0].Uid},"UseraccessToken")
+      // console.log(result[0].Uid);
+      // res.json(accessToken);
+
+      // res.json({status:0});
+      res.json({accessToken });
     }
   });
 
