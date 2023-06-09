394af769671ccc4ee7c4e6b94a17fd1ad4ce2755
Siddhant Raj <siddhantraj04@gmail.com>
2023-06-09 15:11:14 +0530

    Add functionality to like or unlike a post by a valid user

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -14,15 +14,60 @@ const Home = () => {
       // console.log(response.data);
     });
   }, []);
+  const likePost=(post_id)=>{
+    axios.post("http://localhost:3024/like",{Post_ID:post_id},{headers:{
+      accessToken:localStorage.getItem("accessToken")
+    }}).then((response)=>{
+      // console.log(response);
+      if(response.data.error)
+      {
+        alert(response.data.error);
+        return;
+      }
+      if(response.data.liked===true)
+      {
+        alert("You liked a post!");
+        setListofposts(listofpost.map((post)=>{
+          if(post.id===post_id)
+          {
+            return{...post,likes_count:post.likes_count+1};
+          }
+          else
+          {
+            return post;
+          }
+        }))
+      }
+      else
+      {
+        alert("You disliked a post!");
 
+        setListofposts(listofpost.map((post)=>{
+          if(post.id===post_id)
+          {
+            return{...post,likes_count:post.likes_count-1};
+          }
+          else
+          {
+            return post;
+          }
+        }))
+      }
+      
+    })
+  }
   return (
     <>
       {listofpost.map((post) => (
         <>
-          <div className="post" onClick={() => navigate(`/post/${post.id}`)}>
-            <div className="title">{post.title}</div>
-            <div className="body">{post.postText}</div>
-            <div className="footer">{post.username}</div>
+          <div className="post" >
+            <div className="title" onClick={() => navigate(`/post/${post.id}`)}>{post.title}</div>
+            <div className="body" onClick={() => navigate(`/post/${post.id}`)}>{post.postText}</div>
+            <div className="footer">
+              {post.username}
+              <button onClick={()=>{likePost(post.id)}}>Like</button>
+              <span>{post.likes_count}</span>
+              </div>
           </div>
         </>
       ))}
